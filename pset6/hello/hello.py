07a2c080933a4abb0e9193abf0bcc9241b6c346e
Senay Yakut <senaykt@gmail.com>
2020-07-12 18:32:28 -0700

    pset6 hello.py

---
+++ pset6/hello/hello.py
@@ -0,0 +1,12 @@
+'''
+Implement a program that prints out a simple greeting to the user, per the below.
+
+$ python hello.py
+What is your name?
+David
+hello, David
+
+'''
+
+name = input("What is your name?\n")
+print("hello,", name.capitalize())
