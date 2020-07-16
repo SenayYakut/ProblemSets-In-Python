45adcfebadc07d4d75e16b6d0cf63da36779a707
Senay Yakut <senaykt@gmail.com>
2020-07-15 19:49:09 -0700

    Dictionary comhrehension in Python

---
+++ dictComprehension.py
@@ -0,0 +1,10 @@
+'''
+Write a program in Python which will create a dictionary each item with certain range will be paired key as itself and a value as its square using dictionary comprehention 
+'''
+squares = {}
+for x in range(6):
+    squares[x] = x*x
+print(squares)
+
+    
+    
