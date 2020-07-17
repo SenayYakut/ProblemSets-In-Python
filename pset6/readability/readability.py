eb137da0e548913d8ceff0df0a71e51c9dcfd89a
Senay Yakut <senaykt@gmail.com>
2020-07-16 17:30:19 -0700

    Solution 2 is only working, Solution 1 will work any compiler

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -1,5 +1,5 @@
 
-
+# Solution 1
 text = input("Text: ")
 letter_count = 0
 word_count = 1
@@ -7,7 +7,7 @@ sentences_count = 0
 l = len(text)
 
 for i in range(l):
-    if text[l].isalnum() == True:
+    if text[i].isalnum() == True:
         letter_count = letter_count +1
 for i in range(l):
     if text[i] == " " and text[i + 1].isalnum() ==True:
@@ -27,3 +27,33 @@ elif grades > 16:
 else:
     print(f"Grade {grades}")        
 
+
+# Solution 2
+from cs50 import get_string
+
+text = get_string("Text: ")
+letter_count = 0
+word_count = 1
+sentences_count = 0
+l = len(text)
+
+for i in range(l):
+    if text[i].isalnum() == True:
+        letter_count = letter_count + 1
+for i in range(l):
+    if text[i] == " " and text[i + 1].isalnum() == True:
+        word_count = word_count + 1         
+for i in range(l):
+    if text[i] == "." or text[i] == "?" or text[i] == ":" or text[i] == "!":
+        sentences_count = sentences_count + 1
+
+L = 100 * letter_count / word_count
+S = 100 * sentences_count / word_count        
+grades = round(0.0588 * L - 0.296 * S - 15.8)        
+
+if grades < 1:
+    print("Before Grade 1")
+elif grades > 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {grades}") 
