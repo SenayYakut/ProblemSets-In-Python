42ae59ef8a0d685e87519195fe8b834429ec173c
Senay Yakut <senaykt@gmail.com>
2020-07-19 13:19:06 -0700

    Improved solution for dna problem

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -1,23 +1,39 @@
 import csv
 from sys import argv, exit
 
-if len(argv) != 2:
+
+if len(argv) != 3:
     print("missing command_line argument")
     exit(1)
-else:
-    exit(0)
 
-names = []
 
-with open('argv[0]','r') as csvfile:
-    csvReader = csv.reader(csvfile, delimiter=",")
-    for row in csvReader:
-        name = row[0]
-        names.append(name)
 
-with open('argv[1]','r') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=",")
-    for row in readCSV:
-        for i in range(len(row)):
-            row[]
-        AGATC,TTTTTTCT,AATG,TCTAG,GATA,TATC,GAAA,TCTG
+# Opening the csv file and reading its content into the memory
+
+with open(argv[1], "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    dict_list =[]
+    for row in reader:
+        dict_list.append(row)
+# print(dict_list)
+
+with open(argv[2], "r") as txt_file:
+    s = txt_file.read()
+    AGATC = s.count("AGATC")
+    TTTTTTCT = s.count("TTTTTTCT")
+    AATG = s.count("AATG")
+    TCTAG = s.count("TCTAG")
+    GATA = s.count("GATA")
+    TATC = s.count("TATC")
+    GAAA = s.count("GAA")
+    TCTG = s.count("TCTG")
+    dict_l = []
+    dict_l.append(AGATC)
+    dict_l.append(TTTTTTCT)
+    dict_l.append(AATG)
+    dict_l.append(TCTAG)
+    dict_l.append(GATA)
+    dict_l.append(TATC)
+    dict_l.append(GAAA)
+    dict_l.append(TCTG)
+    print(dict_l)
